0d58d0d1c5a09e782e20a1382ae561fcda5c0d5e
doflare123 <144382600+doflare123@users.noreply.github.com>
2025-03-18 22:25:50 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/KodePract/" element={<MainPage />} />
         <Route path="/user/:id" element={<UserDetail />} />
       </Routes>
     </Router>
